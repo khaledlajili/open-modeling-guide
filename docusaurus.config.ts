2ea2b5094502b0251a7d02ba8bff24a8fc7ca4cd
khaledlajili <khaled.lajili@enis.tn>
2026-05-14 18:24:08 +0100

    update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,16 +14,11 @@ const config: Config = {
         v4: true, // Improve compatibility with the upcoming Docusaurus v4
     },
 
-    // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    url: 'https://khaledlajili.github.io',
+    baseUrl: '/open-modeling-guide/',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'khaledlajili', // Usually your GitHub org/user name.
-    projectName: 'open-modeling-guide', // Usually your repo name.
+    organizationName: 'khaledlajili',
+    projectName: 'open-modeling-guide',
 
     onBrokenLinks: 'throw',
 
